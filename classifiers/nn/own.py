556c09b7d899c470a932aec4d013f3a1452635c9
pelonidas <jannahalka@icloud.com>
2024-12-31 13:27:03 +0100

    Add initial NeuralNetwork class template with placeholder methods

---
+++ classifiers/nn/own.py
@@ -0,0 +1,26 @@
+class NeuralNetwork():
+    def __init__(self) -> None:
+        pass
+    def init_params(self):
+        pass
+
+    def sigmoid(self):
+        pass
+
+    def relu(self):
+        pass
+
+    def forward(self):
+        pass
+
+    def cost_fn(self):
+        pass
+
+    def backpropagatio(self):
+        pass
+
+    def update_parameters(self):
+        pass
+
+    def train(self):
+        pass
